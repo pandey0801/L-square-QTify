b5b5832eb844ce0cda90a579be7e748898663eca
vinay pandey <vinaypandey0801gmail.com>
2026-08-02 11:07:34 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Hero from './components/Hero/Hero'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
+      </div> */}
+      {/* <h1>Vite + React</h1> */}
+      <Navbar />
+      {/* <Hero /> */}
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/components/helpers/helpers.js
@@ -0,0 +1,4 @@
+export const truncate = (str, n) => {
+    if (!str) return "";
+    return str.length > n ? str.substring(0, n) + "..." : str;
+};
