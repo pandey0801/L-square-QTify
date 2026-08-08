1d7cd65b2f38bdbee33fac97d639acd4d615fba5
vinay pandey <vinaypandey0801gmail.com>
2026-08-08 17:10:21 +0530

    chnage in the right left carousell

--- src/components/Carousel/CarouselLeftNavigation/CarouselLeftNavigation.jsx
+++ src/components/Carousel/CarouselLeftNavigation/CarouselLeftNavigation.jsx
@@ -1,23 +1,65 @@
+// import React, { useEffect, useState } from "react";
+// import { useSwiper, useSwiperSlide } from "swiper/react";
+// import styles from "./CarouselLeftNavigation.module.css";
+// import LeftArrow from "../../../assets/LeftArrow.svg";
+
+// export default function CarouselLeftNavigation() {
+//     const swiper = useSwiper();
+//     const [isBeginning, setIsBeginning] = useState(swiper.isBeginning);
+
+//     swiper.on("slideChange", function () {
+//         setIsBeginning(swiper.isBeginning);
+//     });
+
+//     return (
+//         <div className={styles.leftNavigation}>
+//             {!isBeginning && <img
+//                 src={LeftArrow}
+//                 alt="Previous"
+//                 onClick={() => swiper.slidePrev()}
+//             />}
+//         </div>
+//     );
+// }
+
+
 import React, { useEffect, useState } from "react";
-import { useSwiper, useSwiperSlide } from "swiper/react";
-import styles from "./CarouselLeftNavigation.module.css";
+import { useSwiper } from "swiper/react";
 import LeftArrow from "../../../assets/LeftArrow.svg";
+import styles from "./CarouselLeftNavigation.module.css";
 
 export default function CarouselLeftNavigation() {
     const swiper = useSwiper();
-    const [isBeginning, setIsBeginning] = useState(swiper.isBeginning);
 
-    swiper.on("slideChange", function () {
-        setIsBeginning(swiper.isBeginning);
-    });
+    const [isBeginning, setIsBeginning] = useState(
+        swiper.isBeginning
+    );
+
+    useEffect(() => {
+        const handleSlideChange = () => {
+            setIsBeginning(swiper.isBeginning);
+        };
+
+        swiper.on("slideChange", handleSlideChange);
+
+        return () => {
+            swiper.off("slideChange", handleSlideChange);
+        };
+    }, [swiper]);
+
+    const handlePrevious = () => {
+        swiper.slidePrev();
+    };
 
     return (
         <div className={styles.leftNavigation}>
-            {!isBeginning && <img
-                src={LeftArrow}
-                alt="Previous"
-                onClick={() => swiper.slidePrev()}
-            />}
+            {!isBeginning && (
+                <img
+                    src={LeftArrow}
+                    alt="Previous"
+                    onClick={handlePrevious}
+                />
+            )}
         </div>
     );
 }
--- src/components/Carousel/CarouselRightNavigation/CarouselRightNavigation.jsx
+++ src/components/Carousel/CarouselRightNavigation/CarouselRightNavigation.jsx
@@ -1,23 +1,63 @@
+// import React, { useEffect, useState } from "react";
+// import { useSwiper } from "swiper/react";
+// import styles from "./CarouselRightNavigation.module.css";
+// import RightArrow from "../../../assets/RightArrow.svg";
+
+// export default function CarouselRightNavigation() {
+//     const swiper = useSwiper();
+//     const [isEnd, setIsEnd] = useState(swiper.isEnd);
+//     swiper.on("slideChange", function () {
+//         setIsEnd(swiper.isEnd);
+//     });
+
+//     return (
+//         <div className={styles.rightNavigation}>
+
+//             {!isEnd && <img
+//                 src={RightArrow}
+//                 alt="Next"
+//                 onClick={() => swiper.slideNext()}
+//             />}
+//         </div>
+//     );
+// } 
+
+
 import React, { useEffect, useState } from "react";
 import { useSwiper } from "swiper/react";
-import styles from "./CarouselRightNavigation.module.css";
 import RightArrow from "../../../assets/RightArrow.svg";
+import styles from "./CarouselRightNavigation.module.css";
 
 export default function CarouselRightNavigation() {
     const swiper = useSwiper();
+
     const [isEnd, setIsEnd] = useState(swiper.isEnd);
-    swiper.on("slideChange", function () {
-        setIsEnd(swiper.isEnd);
-    });
+
+    useEffect(() => {
+        const handleSlideChange = () => {
+            setIsEnd(swiper.isEnd);
+        };
+
+        swiper.on("slideChange", handleSlideChange);
+
+        return () => {
+            swiper.off("slideChange", handleSlideChange);
+        };
+    }, [swiper]);
+
+    const handleNext = () => {
+        swiper.slideNext();
+    };
 
     return (
         <div className={styles.rightNavigation}>
-
-            {!isEnd && <img
-                src={RightArrow}
-                alt="Next"
-                onClick={() => swiper.slideNext()}
-            />}
+            {!isEnd && (
+                <img
+                    src={RightArrow}
+                    alt="Next"
+                    onClick={handleNext}
+                />
+            )}
         </div>
     );
-} 
+}
